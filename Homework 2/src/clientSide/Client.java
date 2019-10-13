c86dfacdeadd7263ebca34e899f9ffafa5a8fb27
vancretec <56044734+vancretec@users.noreply.github.com>
2019-10-13 18:10:36 -0500

    Homework 2

---
+++ Homework 2/src/clientSide/Client.java
@@ -0,0 +1,63 @@
+
+	package clientSide;
+
+	import java.io.BufferedReader;
+
+	import java.io.IOException;
+
+	import java.io.InputStreamReader;
+
+	import java.net.InetSocketAddress;
+
+	import java.net.Socket;
+
+	import javax.swing.JOptionPane;
+
+	/**
+
+	* client for the date server.
+
+	*/
+
+	public class Client {
+
+	    /**
+
+	     * Runs the client application to read date and time from the server side..
+
+	     */
+
+	    public static void main(String[] args) throws IOException {
+
+	        String serverAddress = "";
+
+	        /** To Do
+
+	         * Create a socket and connects it to the specified
+
+	         * port number you created in the server side.
+
+	         * Hint: Use the constructor with host name and port as parameters
+
+	         */
+
+	/* TO DO Here*/
+	        Socket s = new Socket("127.0.0.1" , 7070);
+			BufferedReader input =
+
+	            new BufferedReader(new InputStreamReader(s.getInputStream()));
+
+	        String serverTime = input.readLine();
+	        /** To Do
+
+	         * Write code to disconnect Here.
+
+	         */
+
+	/* TO DO Here*/
+	        s.close();
+	        System.exit(0);
+
+	    }
+
+	}
